ec6a1ca143c52932147f05d5c7b4a46bba1a93cf
Francisco Santos <fwalbergsantos@outlook.com>
2024-08-01 23:40:45 +0000

    first commit

---
+++ src/main/java/com/softwareextension/fleetcontrol/FleetcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.softwareextension.fleetcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwareextension/fleetcontrol/FleetcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareextension.fleetcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
